50d2ff91ae794a2979fbc4d586e3cf0457ea4a70
David Arnold <dar@xoe.solutions>
2018-02-22 14:16:09 -0500

    Just a test

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Just on comment
 
 from . import sales_team
 from .hooks import pre_init_hook
